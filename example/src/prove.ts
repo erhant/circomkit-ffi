74eab5141b5336d7b0bbef40935192b9a760fd40
erhant <erhany96@gmail.com>
2025-03-11 01:31:55 +0300

    added initial benchmarking

--- example/src/prove.ts
+++ example/src/prove.ts
@@ -1,67 +1,78 @@
 import { Circomkit } from "circomkit";
-import { CircomkitFFIBun, CircomkitFFINode, downloadRelease, getLibPath } from "circomkit-ffi";
+import { CircomkitFFIBun, CircomkitFFINode, downloadRelease, getLibPath, isBun } from "circomkit-ffi";
 import { open, load, close } from "ffi-rs";
-import { existsSync } from "fs";
+import { existsSync, readFileSync } from "fs";
 import * as snarkjs from "snarkjs";
+
+const BUILD_CIRCUIT = false;
 
 const circomkit = new Circomkit({
   inspect: false,
 });
-const N = 30;
-const IN = Array.from({ length: N }, (_, i) => i + 1);
 
-const circuitName = `multiplier_${N}`;
-const inputName = "default";
-
-console.info("Building circuit...");
-const buildPath = await circomkit.compile(circuitName, {
-  file: "multiplier",
-  template: "Multiplier",
-  params: [N],
-});
-console.info(`Compiled circuit to ${buildPath}`);
-
-console.info("Exporting input");
-const path = circomkit.input(circuitName, inputName, { in: IN });
-console.info(`Input exported to ${path}`);
-
-console.info("Creating a witness...");
-const witnessPath = await circomkit.witness(circuitName, inputName);
-console.info(`Witness created at ${witnessPath}`);
-
-// console.info("Generating a proof with Arkworks");
+// download the FFI library if it doesn't exist
 const libPath = getLibPath(import.meta.dir);
 if (!existsSync(libPath)) {
   console.info("Downloading FFI library.");
   await downloadRelease(import.meta.dir);
 }
 
-const circomkitFFI = new CircomkitFFIBun(libPath);
-// const circomkitFFI = new CircomkitFFINode(libPath, open, close, load);
+console.log("Using FFI library at", libPath, "for", isBun() ? "Bun" : "Node");
+const circomkitFFI = isBun() ? new CircomkitFFIBun(libPath) : new CircomkitFFINode(libPath, open, close, load);
 
 // const verifierKey: object = JSON.parse(readFileSync(circomkit.path.ofCircuit(circuitName, "vkey"), "utf-8"));
 
-{
-  console.info("Generating a proof with Arkworks");
-  const arkworkOutput = circomkitFFI.arkworks_prove(
-    circomkit.path.ofCircuitWithInput(circuitName, inputName, "wtns"),
-    circomkit.path.ofCircuit(circuitName, "r1cs"),
-    circomkit.path.ofCircuit(circuitName, "pkey")
-  );
+for (const N of [3, 30, 300, 3000, 30000, 300000]) {
+  const IN = Array.from({ length: N }, (_, i) => i + 1);
+  const circuitName = `multiplier_${N}`;
+  const inputName = "default";
 
-  const { proof, publicSignals } = arkworkOutput;
-  console.info("Proof generated:");
-  console.log(proof);
-}
+  if (BUILD_CIRCUIT) {
+    console.info(`Building circuit for multiplier_${N}`);
+    const buildPath = await circomkit.compile(circuitName, {
+      file: "multiplier",
+      template: "Multiplier",
+      params: [N],
+    });
+    console.info(`Compiled circuit to ${buildPath}`);
 
-{
-  console.info("Generating a proof with SnarkJS");
-  const { proof, publicSignals } = await snarkjs.groth16.prove(
-    circomkit.path.ofCircuit(circuitName, "pkey"),
-    circomkit.path.ofCircuitWithInput(circuitName, inputName, "wtns"),
-    undefined,
-    { singleThread: true }
-  );
-  console.info("Proof generated:");
-  console.log(proof);
+    console.info("Exporting input");
+    const path = circomkit.input(circuitName, inputName, { in: IN });
+    console.info(`Input exported to ${path}`);
+
+    console.info("Creating a witness...");
+    const witnessPath = await circomkit.witness(circuitName, inputName);
+    console.info(`Witness created at ${witnessPath}`);
+  }
+
+  console.info(`Proving for multiplier_${N}`);
+
+  {
+    console.time("Generating a proof with Arkworks");
+    const arkworkOutput = circomkitFFI.arkworks_prove(
+      circomkit.path.ofCircuitWithInput(circuitName, inputName, "wtns"),
+      circomkit.path.ofCircuit(circuitName, "r1cs"),
+      circomkit.path.ofCircuit(circuitName, "pkey")
+    );
+    console.timeEnd("Generating a proof with Arkworks");
+
+    // console.info("Proof generated:");
+    // const { proof, publicSignals } = arkworkOutput;
+    // console.log(proof);
+  }
+
+  {
+    console.time("Generating a proof with SnarkJS");
+    const snarkjsOutput = await snarkjs.groth16.prove(
+      circomkit.path.ofCircuit(circuitName, "pkey"),
+      circomkit.path.ofCircuitWithInput(circuitName, inputName, "wtns"),
+      undefined,
+      { singleThread: true }
+    );
+    console.timeEnd("Generating a proof with SnarkJS");
+
+    // console.info("Proof generated:");
+    // const { proof, publicSignals } = snarkjsOutput;
+    // console.log(proof);
+  }
 }
