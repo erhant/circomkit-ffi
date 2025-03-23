e6bf9e0d8c491948f1197e7606f8fda3e5c3e63f
erhant <erhany96@gmail.com>
2025-03-24 00:38:42 +0300

    prep icicle

--- src/icicle/mod.rs
+++ src/icicle/mod.rs
@@ -1,3 +1,36 @@
+use eyre::Result;
+use icicle_snark::{groth16_prove, CacheManager};
+use serde::Serialize;
+use std::path::Path;
+
+use crate::snarkjs::SnarkjsOutput;
+
+const ALLOWED_DEVICES: [&str; 3] = ["CPU", "CUDA", "METAL"];
+
+pub fn prove_with_existing_witness(
+    wtns_path: impl AsRef<Path>,
+    pkey_path: impl AsRef<Path>,
+    device: &str,
+) -> Result<SnarkjsOutput> {
+    let device = device.to_uppercase();
+    if !ALLOWED_DEVICES.contains(&device.as_str()) {
+        return Err(eyre::eyre!("device must be one of {:?}", ALLOWED_DEVICES));
+    }
+
+    let proof_path = "./proof.json".to_string();
+    let public_path = "./public.json".to_string();
+    let proof = groth16_prove(
+        wtns_path.as_ref().as_os_str().to_str().unwrap(),
+        pkey_path.as_ref().as_os_str().to_str().unwrap(),
+        &proof_path,
+        &public_path,
+        &device,
+        &mut CacheManager::default(),
+    );
+
+    todo!("todo")
+}
+
 #[cfg(test)]
 mod tests {
     use icicle_snark::{groth16_prove, CacheManager};
